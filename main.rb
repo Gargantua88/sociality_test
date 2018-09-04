4078c0b6d56e0fa96861c61f8203e14225c3975a
gargantua <istroganovi@gmail.com>
2018-09-04 16:56:28 +0300

    Initial commit

---
+++ main.rb
@@ -0,0 +1,15 @@
+require_relative 'lib/test'
+require_relative 'lib/result_printer'
+
+user_name = ARGV[0]
+user_name = "Тестируемый" if user_name == nil
+
+puts "Здравствуй, #{user_name}! Тест поможет определить твой уровень коммуникабельности.\n"\
+  "Правдиво ответь на следующие вопросы одним из 3 вариантов: \"yes\", \"no\" и \"sometimes\"."
+
+test = Test.new
+result_printer = ResultPrinter.new(user_name)
+
+test.next_question until test.finished?
+
+result_printer.result(test.balls)
