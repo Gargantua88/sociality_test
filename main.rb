be0de255174553cafae7069575b5fb9f9f12a4b4
gargantua <istroganovi@gmail.com>
2018-09-06 11:29:34 +0300

    with files in .txt

--- main.rb
+++ main.rb
@@ -3,13 +3,14 @@ require_relative 'lib/result_printer'
 
 user_name = ARGV[0]
 user_name = "Тестируемый" if user_name == nil
+file_path = File.dirname(__FILE__)
 
 puts "Здравствуй, #{user_name}! Тест поможет определить твой уровень коммуникабельности.\n"\
   "Правдиво ответь на следующие вопросы одним из 3 вариантов: \"yes\", \"no\" и \"sometimes\"."
 
-test = Test.new
-result_printer = ResultPrinter.new(user_name)
+test = Test.new(file_path + '/data/questions.txt')
+result_printer = ResultPrinter.new(user_name, file_path + '/data/answers.txt')
 
 test.next_question until test.finished?
 
-result_printer.result(test.balls)
+result_printer.result(test.points)
